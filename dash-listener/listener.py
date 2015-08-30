c765e3347de2d80fa98212fcc701dffa673abe32
Brett Neese <brett@brettneese.com>
2015-08-30 21:06:26 +0000

    add example init.d script for dash-listener, fix init.d script for airplay-listener

--- dash-listener/listener.py
+++ dash-listener/listener.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from scapy.all import *
 import lircplayback as playback
+import signal
 
 def button_pressed():
     playback.airplay_toggle()
@@ -9,7 +10,15 @@ def button_pressed():
 def arp_display(pkt):
     if pkt[ARP].op == 1: #who-has (request)
         if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
-            if pkt[ARP].hwsrc == '74:c2:46:a8:db:b6': # Huggies
+            if pkt[ARP].hwsrc == '74:c2:46:a8:db:b6': # Gatorade button
                 button_pressed()
 
 sniff(prn=arp_display, filter="arp", store=0)
+
+def sigterm_handler(_signo, _stack_frame):
+    "When sysvinit sends the TERM signal, cleanup before exiting."
+    print("[" + get_now() + "] received signal {}, exiting...".format(_signo))
+    cleanup_pins()
+    sys.exit(0)
+
+signal.signal(signal.SIGTERM, sigterm_handler)
